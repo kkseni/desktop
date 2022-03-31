9acc2e29df65d7ab273a3ab93c0bed0860e32a69
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-03-31 19:33:29 +0300

    +

--- js/menu.js
+++ js/menu.js
@@ -10,3 +10,17 @@
         mobileMenuRef.classList.toggle("is__open");
     });
 })();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-menu-open]'),
+    closeModalBtn: document.querySelector('[data-menu-close]'),
+    modal: document.querySelector('[data-menu]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('is-hidden');
+  }
+})();
